aabd1338c17a9d64b777405d70d1d75a2ddfb4dc
Omar Rida <omar@audiogram.me>
2019-10-19 19:11:09 -0400

    create bio component

---
+++ src/components/bio.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Image from "../components/image"
+
+const Bio = () => {
+  return (
+    <div className={'bg-gray-100 flex items-center p-8'}>
+      <div className={'w-24'}>
+        <Image />
+      </div>
+      <div className={'flex-1 p-2'}>
+        <h1 className={'font-sans text-2xl m-0'}>Omar Rida</h1>
+        <p className={'font-sans text-sm text-gray-600 mb-0'}>Senior Engineer at RealBlocks</p>
+        <p className={'font-sans text-sm mt-2 leading-snug mb-0'}>I’m a founder, programmer, and product designer. Earlier this year I joined RealBlocks.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Bio
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,40 +7,19 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <main>{children}</main>
+      <footer>
+        <p className={"font-sans text-sm mb-0 text-center"}>
+          Made with <a href={"https://www.gatsbyjs.org"} className={"text-blue-600"}>Gatsby.js</a> and <a href={"https://tailwindcss.com"} className={"text-blue-600"}>Tailwind CSS</a>
+        </p>
+      </footer>
     </>
   )
 }
