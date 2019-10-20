b21f8afe8ff43757af1472af50dd2ac1208c7d0b
Omar Rida <omar@audiogram.me>
2019-10-20 19:32:38 -0400

    improve blog post resonsiveness on large and xlarge screens

--- src/components/blog-post.js
+++ src/components/blog-post.js
@@ -3,11 +3,11 @@ import RealBlocksLogo from "../components/realblocks-logo"
 
 function BlogPost({ title, description, medium }) {
   return (
-    <div className={"flex items-center p-8 pt-0"}>
+    <div className={"flex items-center p-8 pt-0 xl:flex-none xl:inline-block xl:w-1/3"}>
       <div className="max-w-sm w-full lg:max-w-full">
         <a href={medium} target="_blank">
           <div
-            className="lg:w-2/3 shadow border-r border-b border-l border-t border-gray-400 lg:border-l lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+            className="lg:w-2/3 xl:w-full shadow border-r border-b border-l border-t border-gray-400 lg:border-l lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b lg:rounded-r p-4 flex flex-col justify-between leading-normal">
             <div>
               <div className="text-gray-900 font-sans font-bold text-sm mb-2">{title}</div>
               <p className="text-gray-700 font-sans text-sm">{description}</p>
